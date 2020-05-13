bfca88063e03200ae4ea16d3507d10b8d06211ec
evmcodi <a@a.com>
2020-05-13 05:15:02 +0100

    Fix bug 'The "dnspython" module must be installed to use mongodb+srv:// URIs'

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+app.config["MONGO_URI"] = os.getenv('MONGO_URI')
+# , 'mongodb://localhost'
 
 mongo = PyMongo(app)
 
